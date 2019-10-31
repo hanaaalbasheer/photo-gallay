82cffe9ab3ce33110f0cf5e4e604e137d2b37feb
hana_basheer <hanaaalbasheer@gmail.com>
2019-10-31 02:12:27 -0700

    ha

--- gal.js
+++ gal.js
@@ -21,3 +21,68 @@ function gallarey(x){
     if(x < 1){ index = images.length}
     images[index-1].style.display = "block";
 }
+let array = [];
+function get()
+{
+    let id = document.getElementById("id").value;
+           let id1='';
+         if(id !== ""){
+           id1="/"+id;
+         }
+    let Fname = document.getElementById("first_name").value;
+    if (Fname !== '') {
+        array.push('first_name='+ Fname)
+    }
+
+    let lname = document.getElementById("last_name").value;
+    if (lname !== '') {
+        array.push('last_name='+ Fname)
+    }
+    let page = document.getElementById("PN").value;
+    if (page !== '') {
+        array.push('page='+page)
+    }
+    let y=id1+ '?' + array.join('&')
+    console.log(y);
+    const obj = new Headers();
+    obj.append('Authorization','Bearer YYcizpb-E7TuZKyRzxr3Dm5MuPvAZKDLTXP_');
+    obj.append('Content-Type', 'application/json');
+    fetch('https://gorest.co.in/public-api/photos  '+y, {
+            method: 'GET',
+            headers: obj
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+
+document.getElementById("button2").addEventListener("click",post);
+
+function post()
+{
+    let Fname = document.getElementById("first_name").value;
+
+    let lname = document.getElementById("last_name").value;
+
+    let email = document.getElementById("email").value;
+    
+    let gender = document.getElementById("gender").value;
+   
+    const obj = new Headers();
+    obj.append('Authorization','Bearer YYcizpb-E7TuZKyRzxr3Dm5MuPvAZKDLTXP_');
+    obj.append('Content-Type', 'application/json');
+    fetch('https://gorest.co.in/public-api/users', {
+            method: 'POST',
+            headers: obj,
+            body:JSON.stringify({
+             first_name:Fname,
+             last_name:lname,
+             email:email,
+             gender:gender,
+
+
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
